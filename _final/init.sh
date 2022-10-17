897f2d5019d370b6829258a4542a357b1928d0b2
Mikayel Galyan <mikayel.galyan@gmail.com>
2022-10-17 18:00:52 +0400

    latest

--- _final/init.sh
+++ _final/init.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-echo "Please input variables or push Enther for default values"
-read -p "Please input: AWS_PROFILE: Default: default: " AWS_PROFILE
-read -p "Please input: AWS_REGION: Default: us-east-1: " AWS_REGION
-read -p "Please input: CLUSTER_NAME: Default: zerosystems-cluster: " CLUSTER_NAME
-read -p "Please input CIDR for VPC: Default: 10.8.0.0/16: " CIDR
-read -p "Do You use AWS GovCloud? type no or yes: Default: No: " GovCloud 
+echo "Please enter variables or press Enter for default values"
+read -p "Please enter: AWS_PROFILE: Default: default: " AWS_PROFILE
+read -p "Please enter: AWS_REGION: Default: us-east-1: " AWS_REGION
+read -p "Please enter: CLUSTER_NAME: Default: zerosystems-cluster: " CLUSTER_NAME
+read -p "Please enter: CIDR for VPC: Default: 10.8.0.0/16: " CIDR
+read -p "Are you using AWS GovCloud? Enter no or yes: Default: No: " GovCloud 
 
-read -p "Please find in https://docs.aws.amazon.com/eks/latest/userguide/add-ons-images.html this number: " NUMBER_OF_IMAGE
+read -p "Please look at https://docs.aws.amazon.com/eks/latest/userguide/add-ons-images.html for the first number of registry: " NUMBER_OF_IMAGE
 read -p "Please enter certificate ARN: " CERT_ARN
 read -p "Please enter secret name: " SECRET_NAME
 
@@ -18,10 +18,10 @@ export CIDR=${CIDR:-"10.8.0.0/16"}
 GovCloud=${GovCloud:-"no"}
 
 echo "-----------------"
-echo "Profile name is $AWS_PROFILE"
-echo "Region is $AWS_REGION"
-echo "Cluster name is $CLUSTER_NAME"
-echo "CIDR is $CIDR"
+echo "PROFILE - $AWS_PROFILE"
+echo "REGION - $AWS_REGION"
+echo "Cluster - $CLUSTER_NAME"
+echo "CIDR - $CIDR"
 echo "-----------------"
 
 cp --remove-destination zero-eksctl-template.yaml eksctl.yaml
@@ -36,8 +36,8 @@ export VPC_ID=$(aws ec2 describe-vpcs --region ${AWS_REGION} --filter Name=tag:N
 export AWS_ACCOUNT=$(aws sts get-caller-identity --profile ${AWS_PROFILE} --query 'Account' --output text)
 
 echo "-----------------"
-echo "VPC_ID name is $VPC_ID"
-echo "AWS_ACCOUNT is $AWS_ACCOUNT"
+echo "VPC_ID - $VPC_ID"
+echo "AWS_ACCOUNT - $AWS_ACCOUNT"
 echo "-----------------"
 
 if [ "$GovCloud" = "yes" ]
@@ -59,9 +59,9 @@ eksctl create iamserviceaccount \
 
 kubectl annotate serviceaccount -n kube-system aws-load-balancer-controller eks.amazonaws.com/sts-regional-endpoints=true 
 
-helm repo add eks https://aws.github.io/eks-charts
-helm repo add secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
-helm repo add zerosystemsapi https://charts.zerosystems.sbs
+helm repo add --force-update eks https://aws.github.io/eks-charts
+helm repo add --force-update secrets-store-csi-driver https://kubernetes-sigs.github.io/secrets-store-csi-driver/charts
+helm repo add --force-update zerosystemsapi https://charts.zerosystems.sbs
 helm repo update
 
 helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
@@ -96,20 +96,21 @@ POLICY_ARN=$(aws --query Policy.Arn --output text iam create-policy --policy-nam
 
 eksctl create iamserviceaccount --name secretsmanager-policy-sa --cluster ${CLUSTER_NAME} --attach-policy-arn "$POLICY_ARN" --approve --override-existing-serviceaccounts
 
-helm install ${CLUSTER_NAME} zerosystemsapi/zeroconnectapi --version 1.9.0 --atomic --set secretsName=${SECRET_NAME} --set ingress.certificateARN=${CERT_ARN} --set image.tag=1.9.0.41
+helm install zerosystemsapi zerosystemsapi/zeroconnectapi --version 1.9.0 --set secretsName=${SECRET_NAME} --set ingress.certificateARN=${CERT_ARN} --set image.tag=1.9.0.41
 
 
 echo "#!/usr/bin/env bash" > ./delete.sh
 echo "export AWS_PROFILE=${AWS_PROFILE}" >> ./delete.sh
 echo "export AWS_REGION=${AWS_REGION}" >> ./delete.sh
-echo "helm uninstall ${CLUSTER_NAME}" >> ./delete.sh
+echo "helm uninstall zerosystemsapi" >> ./delete.sh
 echo "eksctl delete iamserviceaccount --name secretsmanager-policy-sa --cluster ${CLUSTER_NAME}" >> ./delete.sh
-echo "aws iam delete-policy --policy-arn ${POLICY_ARN}" >> ./delete.sh
 echo "kubectl delete -f https://raw.githubusercontent.com/aws/secrets-store-csi-driver-provider-aws/main/deployment/aws-provider-installer.yaml" >> ./delete.sh
 echo "helm uninstall -n kube-system csi-secrets-store" >> ./delete.sh
-echo "helm install -n kube-system aws-load-balancer-controller" >> ./delete.sh
+echo "helm uninstall -n kube-system aws-load-balancer-controller" >> ./delete.sh
 echo "eksctl delete iamserviceaccount --name aws-load-balancer-controller --cluster=${CLUSTER_NAME}" >> ./delete.sh
-echo "aws iam delete-policy --policy-arn ${POLICY_ALB_ARN}" >> ./delete.sh
 echo "eksctl delete cluster -f ./eksctl.yaml" >> ./delete.sh
+echo "aws iam delete-policy --policy-arn ${POLICY_ARN}" >> ./delete.sh
+echo "aws iam delete-policy --policy-arn ${POLICY_ALB_ARN}" >> ./delete.sh
+echo "helm repo remove eks secrets-store-csi-driver zerosystemsapi" >> ./delete.sh
 
 chmod +x ./delete.sh
